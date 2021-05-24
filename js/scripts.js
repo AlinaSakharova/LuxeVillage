f8d958cf23a6f034f8096d520de15d981d3fa560
AlinaSakharova <75130764+AlinaSakharova@users.noreply.github.com>
2021-05-24 12:45:05 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(function() {
+    let header = $('.header');
+    let hederHeight = header.height(); // вычисляем высоту шапки
+     
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 1) {
+       header.addClass('header_fixed');
+       $('body').css({
+          'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+       });
+      } else {
+       header.removeClass('header_fixed');
+       $('body').css({
+        'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+       })
+      }
+      if($(this).scrollTop() > 300) {
+        header.css({
+          'padding': '5px 0',
+          'background': '#fffff',
+          'transition': '.3s'
+        });
+    } else {
+        header.css({
+          'padding': '15px 0',
+          'background': '#ffffff',
+          'transition': '.3s'
+        });
+    }
+    });
+   });
